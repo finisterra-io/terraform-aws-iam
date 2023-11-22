814655df03039dcecd49a28c00bbfcb8546e0181
xneyder <xneyder@gmail.com>
2023-11-21 20:55:56 -0500

    fix

--- modules/iam_role/main.tf
+++ modules/iam_role/main.tf
@@ -21,6 +21,7 @@ resource "aws_iam_instance_profile" "default" {
   for_each = var.enabled ? { for role in var.instance_profiles : role.name => role } : {}
   name     = each.key
   role     = join("", aws_iam_role.default.*.name)
+  path     = try(each.value.path, null)
   tags     = each.value.tags
 }
 
