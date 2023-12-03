08353cac1a6e67fa8e8d711c90805a4b81857d95
xneyder <xneyder@gmail.com>
2023-12-03 11:00:44 -0500

    fix

--- modules/iam_role/outputs.tf
+++ modules/iam_role/outputs.tf
@@ -12,13 +12,3 @@ output "arn" {
   value       = join("", aws_iam_role.default.*.arn)
   description = "The Amazon Resource Name (ARN) specifying the role"
 }
-
-output "policy" {
-  value       = join("", data.aws_iam_policy_document.default.*.json)
-  description = "Role policy document in json format. Outputs always, independent of `enabled` variable"
-}
-
-output "instance_profile" {
-  description = "Name of the ec2 profile (if enabled)"
-  value       = join("", aws_iam_instance_profile.default.*.name)
-}
